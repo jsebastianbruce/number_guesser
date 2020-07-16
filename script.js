94a5875c9271506243643ed2da91af33188744b8
Juan Bruce <j.sebastianbruce@gmail.com>
2020-07-15 23:22:13 -0500

    Project requirements completed

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random * 10)
+};
+
+const compareGuesses = (user, computer, secret) => {
+    const userDifference = Math.abs(secret - user);
+    const computerDifference = Math.abs(secret - computer);
+    return user <= computer;
+};
+
+const updateScore = string => {
+    if (string === 'human') {
+        humanScore++;
+    } else if (string === 'computer') {
+        computerScore++;
+    }
+};
+
+const advanceRound = () => {
+    currentRoundNumber++;
+}
